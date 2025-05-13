30773a3ea085e2ce779e9e65b069ae86d9ab0707
Federico Gutman <fgutman@udesa.edu.ar>
2025-05-13 19:36:13 -0300

    cambios

--- TP3-FileSystem/file.c
+++ TP3-FileSystem/file.c
@@ -9,7 +9,32 @@
  * TODO
  */
 int file_getblock(struct unixfilesystem *fs, int inumber, int blockNum, void *buf) {
+	struct inode inode1;
+	int err = inode_iget(fs, inumber, &inode1);
+	if (err < 0) {
+        return -1;
+    }
+
+	int sector = inode_indexlookup(fs, &inode1, blockNum);
+	if (sector < 0) {
+        return -1;
+    }
+
+	int read_err = diskimg_readsector(fs->dfd, sector, buf);
+	if (read_err < 0) {
+        return -1;
+    }
+
+    int total_bytes = inode_getsize(&inode1);
+	if (total_bytes < 0) {
+        return -1;
+    }
     
-    return 0;
+	int total_blocks = total_bytes / DISKIMG_SECTOR_SIZE;
+	if (blockNum == total_blocks) {
+		return total_bytes % DISKIMG_SECTOR_SIZE;
+	} else {
+		return DISKIMG_SECTOR_SIZE;
+	}
 }
 
