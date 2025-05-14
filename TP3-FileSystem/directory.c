a3c1a4015b941ea4a63ea05babfc3eb302afe0eb
fedegutman <fgutman@udesa.edu.ar>
2025-05-14 11:17:28 -0300

    cambios

--- TP3-FileSystem/directory.c
+++ TP3-FileSystem/directory.c
@@ -10,41 +10,40 @@
  * TODO
  */
 int directory_findname(struct unixfilesystem *fs, const char *name,
-		int dirinumber, struct direntv6 *dirEnt) {
+  int dirinumber, struct direntv6 *dirEnt) {
 
-    struct inode dirinode;
-    int err = inode_iget(fs, dirinumber, &dirinode);
-    if (err < 0) {
-      return -1;
-    }
-
-    if ((dirinode.i_mode & IALLOC) == 0 || (dirinode.i_mode & IFMT) != IFDIR) { // me fijo si el inode es un directorio
-      return -1;
-    }
-
-    int filesize = inode_getsize(&dirinode);
-    if (filesize < 0 || filesize == 0) {
-      return -1;
-    }
-
-    int block_count = (filesize + DISKIMG_SECTOR_SIZE - 1) / DISKIMG_SECTOR_SIZE;
-
-    for (int i = 0; i < block_count; i++) { // recorro todos los bloques del directorio
-      unsigned char buffer[DISKIMG_SECTOR_SIZE];
-      int bytes = file_getblock(fs, dirinumber, i, buffer);
-      if (bytes < 0) return -1;
-
-      int entries = bytes / sizeof(struct direntv6); // calculo el numero de entradas en el bloque
-      struct direntv6 *entry = (struct direntv6 *)buffer;
-
-      for (int j = 0; j < entries; j++) { // recorro todas las entradas del bloque
-          // comparo el nombre de la entrada con el nombre que busco
-          if (strncmp(entry[j].d_name, name, 256) == 0) { // CHEQUEAR LOS 256
-              *dirEnt = entry[j];
-              return 0;
-          }
-      }
+  struct inode inodo;
+  int err = inode_iget(fs, dirinumber, &inodo);
+  if (err < 0) {
+    return -1;
   }
 
+  if ((inodo.i_mode & IALLOC) == 0 || (inodo.i_mode & IFMT) != IFDIR) { // me fijo si el inode es un directorio
+    return -1;
+  }
+
+  int filesize = inode_getsize(&inodo);
+  if (filesize < 0 || filesize == 0) {
+    return -1;
+  }
+
+  int block_count = (filesize + DISKIMG_SECTOR_SIZE - 1) / DISKIMG_SECTOR_SIZE;
+
+  for (int i = 0; i < block_count; i++) { // recorro todos los bloques del directorio
+    unsigned char buffer[DISKIMG_SECTOR_SIZE];
+    int bytes = file_getblock(fs, dirinumber, i, buffer);
+    if (bytes < 0) return -1;
+
+    int entries = bytes / sizeof(struct direntv6); // calculo el numero de entradas en el bloque
+    struct direntv6 *entry = (struct direntv6 *)buffer;
+
+    for (int j = 0; j < entries; j++) { // recorro todas las entradas del bloque
+      // comparo el nombre de la entrada con el nombre que busco
+      if (strncmp(entry[j].d_name, name, 256) == 0) { // CHEQUEAR LOS 256
+          *dirEnt = entry[j];
+          return 0;
+      }
+    }
+  }
   return -1;
 }
