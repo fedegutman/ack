3f208754d3078e89200af54a1997bd2f05fca551
fedegutman <fgutman@udesa.edu.ar>
2025-05-14 10:42:37 -0300

    inodes

--- TP3-FileSystem/inode.c
+++ TP3-FileSystem/inode.c
@@ -21,15 +21,21 @@ int inode_iget(struct unixfilesystem *fs, int inumber, struct inode *inp) {
   int sector_to_read = INODE_START_SECTOR + sector; // busco el sector a leer
 
   int fd = fs->dfd; // obtengo el file descriptor del filesystem
-  struct inode inodes[inodes_per_sector]; // armo una lista para poner todos los inodes
+  struct inode *inodes = malloc(DISKIMG_SECTOR_SIZE); // reservo memoria para los inodes
+
+  if (inodes == NULL) {
+    return -1;
+  }
 
   int err = diskimg_readsector(fd, sector_to_read, inodes); // leo el sector
 
   if (err < 0){
+    free(inodes);
     return -1;
   }
   
   *inp = inodes[inode_position]; // me guardo el inode que buscaba
+  free(inodes);
   return 0;
 }
 
@@ -37,59 +43,65 @@ int inode_iget(struct unixfilesystem *fs, int inumber, struct inode *inp) {
  * TODO
  */
 int inode_indexlookup(struct unixfilesystem *fs, struct inode *inp, int blockNum) {  
-  // para acceder al bloque de datos, hay 3 casos posibles que se pueden dar:
-    // 1. el inode apunta directamente al bloque de datos (direct addressing)
-    // 2. el inode apunta a un bloque que tiene numeros otros de bloques (singly indirect blocks)
-    // 3. el inode apunta a un bloque que apunta a otro bloque que tiene numeros de bloques (doubly indirect blocks)
-
-  // caso 1: el archivo es chico por ende el inode apunta directamente a los bloques de datos
   if (blockNum < 0 || inp == NULL) {
     return -1;
   }
   int fd = fs->dfd;
-	int adressing = ((inp->i_mode & ILARG) == 0); // chequeo si el inode usa direct addressing
-	if (adressing) {
-		return inp->i_addr[blockNum]; // devuelvo el bloque de datos
-	}
-
-  // sino entro al if, es porque el archivo es demasiado grande por ende el inode apunta indirectamente a los bloques de datos
-
-	int n_addresses = DISKIMG_SECTOR_SIZE / sizeof(uint16_t); // cantidad de direcciones que puede tener un bloque
-	int singly_indirect_blocks = n_addresses * 7; // obtengo la cantidad de bloques indirectos que puede tener el inode
-                                    // 7 porque el inode tiene 7 bloques singly indirect y 1 bloque doubly indirect
-
-	if (blockNum < singly_indirect_blocks) { // caso 2: el bloque se encuentra en un bloque singly indirect
-		int ind_block = blockNum / n_addresses; // obtengo el bloque de indireccionamiento
-		int ind_block_index = blockNum % n_addresses; // obtengo el indice donde se encuentra el bloque de datos
-		uint16_t addrs[n_addresses];
-
-		int err = diskimg_readsector(fd, inp->i_addr[ind_block], addrs);
-		if (err < 0) {
-      return -1;	
-    }
-		return addrs[ind_block_index];
+  int addressing = ((inp->i_mode & ILARG) == 0);
+  if (addressing) {
+    return inp->i_addr[blockNum];
   }
 
-    // caso 3: el bloque se encuentra en un bloque doubly indirect. tengo que leer un bloque y despues el otro.
-		// entro primero a un bloque y despues al segundo
-		int nblock = blockNum - singly_indirect_blocks;
-		int double_ind_block_index1 = nblock / n_addresses;
-		uint16_t addresses1[n_addresses];
-		int block1_err = diskimg_readsector(fd, inp->i_addr[7], addresses1);
-		if (block1_err < 0) {
+  int n_addresses = DISKIMG_SECTOR_SIZE / sizeof(uint16_t);
+  int singly_indirect_blocks = n_addresses * 7;
+
+  if (blockNum < singly_indirect_blocks) {
+    int ind_block = blockNum / n_addresses;
+    int ind_block_index = blockNum % n_addresses;
+    uint16_t *addrs = malloc(DISKIMG_SECTOR_SIZE);
+    if (addrs == NULL) {
       return -1;
     }
-
-		int sector_2 = addresses1[double_ind_block_index1];
-		int double_ind_block_index2 = nblock % n_addresses;
-		uint16_t addresses2[n_addresses];
-		int block2_err = diskimg_readsector(fd, sector_2, addresses2);
-		if(block2_err < 0) {
-      return -1;
+    int err = diskimg_readsector(fd, inp->i_addr[ind_block], addrs);
+    if (err < 0) {
+      free(addrs);
+      return -1;	
     }
+    int result = addrs[ind_block_index];
+    free(addrs);
+    return result;
+  }
 
-		return addresses2[double_ind_block_index2];
-	}	
+  int nblock = blockNum - singly_indirect_blocks;
+  int double_ind_block_index1 = nblock / n_addresses;
+  uint16_t *addresses1 = malloc(DISKIMG_SECTOR_SIZE);
+  if (addresses1 == NULL) {
+    return -1;
+  }
+  int block1_err = diskimg_readsector(fd, inp->i_addr[7], addresses1);
+  if (block1_err < 0) {
+    free(addresses1);
+    return -1;
+  }
+
+  int sector_2 = addresses1[double_ind_block_index1];
+  free(addresses1);
+
+  int double_ind_block_index2 = nblock % n_addresses;
+  uint16_t *addresses2 = malloc(DISKIMG_SECTOR_SIZE);
+  if (addresses2 == NULL) {
+    return -1;
+  }
+  int block2_err = diskimg_readsector(fd, sector_2, addresses2);
+  if (block2_err < 0) {
+    free(addresses2);
+    return -1;
+  }
+
+  int result = addresses2[double_ind_block_index2];
+  free(addresses2);
+  return result;
+}	
 
 int inode_getsize(struct inode *inp) {
   return ((inp->i_size0 << 16) | inp->i_size1); 
